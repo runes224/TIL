a8e891d522b31bdbb79e978a7c1db841845f99cd
hitakarento <hitakarento@gmail.com>
2020-02-08 09:05:21 +0900

    TIL

---
+++ TIL/src/rento/hitaka/til/Date.java
@@ -0,0 +1,22 @@
+package rento.hitaka.til;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Date {
+
+  public static void main(String[] args) {
+    
+    LocalDate date = LocalDate.now();
+    System.out.println(date);
+    
+    LocalTime time = LocalTime.now();
+    System.out.println(time);
+    
+    LocalDateTime dateTime = LocalDateTime.now();
+    System.out.println(dateTime);
+
+  }
+
+}
